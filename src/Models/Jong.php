c90f12f636f2e7ea5903f588de21aa9e93d634d7
Love Coding <fedde198212@gmail.com>
2021-05-01 11:00:34 -0400

    wip

---
+++ src/Models/Jong.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace LovecodingGit\FirstPackage\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Jong extends Model
+{
+    public function test()
+    {
+        return "test";
+    }
+}
